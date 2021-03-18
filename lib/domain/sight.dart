569e6c8a03818154f2d68ce22fc353468ec81e5e
gommu <kharlap.d@gmail.com>
2021-03-19 01:30:12 +0300

    Task 4.6.1

---
+++ lib/domain/sight.dart
@@ -0,0 +1,22 @@
+class Sight {
+  final String name;
+  final double lat;
+  final double lon;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight({
+    this.name,
+    this.lat,
+    this.lon,
+    this.url,
+    this.details,
+    this.type,
+  });
+
+  @override
+  String toString() {
+    return '$name: $type';
+  }
+}
